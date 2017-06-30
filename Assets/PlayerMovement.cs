2e61dfa498da161dc76e1830c95e7f74f24d9295
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-06-29 23:22:57 -0400

    Initial Build

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+            switch (cameraRaycaster.layerHit)
+            {
+                 case Layer.Walkable:
+                 m_Character.Move(cameraRaycaster.hit.point - transform.position, false, false);
+                 break;
+             }
+        }
+          
+    }
+}
+
