0ec59839abf837cf1c243b9b247ba0cb86656792
Ben Albert <bcalbert777@gmail.com>
2017-07-05 12:12:00 -0400

    update move / cursor

---
+++ Assets/Camera&UI/CursorAffordance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    // Use this for initialization
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D AttackCursor = null;
+	[SerializeField] Texture2D UnkownCursor = null;
+	[SerializeField] Vector2 cursorHotspot = new Vector2 (96, 96);
+
+    CameraRaycaster CameraRaycaster; 
+
+    void Start () {
+        CameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (CameraRaycaster.layerHit)
+		{case Layer.Walkable:
+			Cursor.SetCursor (walkCursor, cursorHotspot, CursorMode.Auto);
+			break;
+		case Layer.Enemy:
+			Cursor.SetCursor (AttackCursor, cursorHotspot, CursorMode.Auto);
+			break;
+		case Layer.RaycastEndStop:
+			Cursor.SetCursor (UnkownCursor, cursorHotspot, CursorMode.Auto);
+			break;
+		default:
+			Debug.LogError ("Dont Know what cursor to show");
+			return;
+		}
+        //Debug.Log(CameraRaycaster.layerHit);
+	}
+}
